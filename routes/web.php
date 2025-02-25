99e10aa8cc2563daec5d6bdd947e765ec7099b0a
user638726 <lienjackson35@gmail.com>
2025-02-25 14:28:11 +0800

    增加主頁面

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\BusController;
 
 //Route::get('/', function () {
-  //  return view('students.index');
+    //return redirect()->route('bus.index');
 //});
-Route::get('/student_index', [StudentController::class, 'index']);
-Route::get('/student_hello', [StudentController::class, 'hello']);
-Route::get('/student_excel', [StudentController::class, 'excel']);
-Route::get('/student_show', [StudentController::class, 'show']);
-Route::resource('students', StudentController::class);
+//Route::get('/student_index', [StudentController::class, 'index']);
+//Route::get('/student_hello', [StudentController::class, 'hello']);
+//Route::get('/student_excel', [StudentController::class, 'excel']);
+//Route::get('/student_show', [StudentController::class, 'show']);
+//Route::resource('students', StudentController::class);
+Route::get('/bus_index', [BusController::class, 'index']);
+Route::resource('bus', BusController::class);
